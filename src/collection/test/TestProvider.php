fc84598554c95dae46a8834812a96aba38227f97
Frangeris Peguero <frangeris.20@gmail.com>
2015-04-28 16:09:21 -0400

    TestProvider refactor

--- src/collection/test/TestProvider.php
+++ src/collection/test/TestProvider.php
@@ -1,33 +1,68 @@
 <?php
 
-namespace Providers\Test;
+namespace Provider\Collection\Test;
 
-use Skeleton\SDK\Providers\AbstractProvider,
-	Skeleton\SDK\Common\Supplier\ISupplier
-	;
+use Skeleton\SDK\Common\Supplier\ISupplier;
+use Skeleton\SDK\Common\Client;
+use Skeleton\SDK\Providers\AbstractProvider;
 
 /**
- * Mock provider test class
- */
+ * Provider for resource /test
+ */            
 class TestProvider extends AbstractProvider implements ISupplier
 {
-	public function create($provider)
-	{
-		if (!is_array($provider))
-			$provider = $this->skeleton->fragmen($provider);
+    /**
+     * Create new resource
+     * 
+     * @param object|array  New resource data to create
+     * @return GuzzleHttp\Message\Response Response from guzzle
+     */
+    public function create($entity)
+    {
+        return $this->skeleton->post('/test', $entity);
+    }
 
-		return $this->skeleton->post('/users', $provider);
-	}
+    /**
+     * Get all from resource
+     * 
+     * @return GuzzleHttp\Message\Response Response from guzzle
+     */
+    public function read()
+    {
+        return $this->skeleton->get('/test');
+    }
 
-	public function read()
-	{}
+    /**
+     * Update the resource
+     *
+     * @param string  Resource Id
+     * @param object|array  Data to update
+     * @return GuzzleHttp\Message\Response Response from guzzle
+     */
+    public function update($id, $entity)
+    {
+        return $this->skeleton->put('/test/'.$id, $entity);
+    }
 
-	public function update($provider)
-	{}
+    /**
+     * Delete a resource
+     *
+     * @param string  Resource id
+     * @return GuzzleHttp\Message\Response Response from guzzle
+     */
+    public function delete($id)
+    {
+        return $this->skeleton->delete('/test/'.$id);
+    }
 
-	public function delete($id)
-	{}
-
-	public function getById($id)
-	{}
+    /**
+     * Get a resource by id
+     *
+     * @param string  Resource id
+     * @return GuzzleHttp\Message\Response  Response from guzzle
+     */
+    public function getById($id)
+    {
+        return $this->skeleton->get('/test/'.$id);
+    }    
 }
